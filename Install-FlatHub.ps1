372be2ac1689e22120885893fbbe5866d01a61cf
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-08-13 14:52:02 -0300

    Reformatando o código fonte.

--- Install-FlatHub.ps1
+++ Install-FlatHub.ps1
@@ -1,4 +1,5 @@
 using module "./Add-FlatpakRepository.ps1"
 function Install-FlatHub {
+
     Add-FlatpakRepository -name "flathub" -location "https://flathub.org/repo/flathub.flatpakrepo"
 }
--- Install-Flatpak.ps1
+++ Install-Flatpak.ps1
@@ -1,5 +1,6 @@
 using module "./Install-DnfPackage.ps1"
 
 function Install-Flatpak {
+    
     Install-DnfPackage -package "flatpak"
 }
