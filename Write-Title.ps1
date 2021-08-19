c98224ec995088b867365a56192ad8555d8cbc7e
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-08-18 21:57:11 -0300

    Criando a função Write-Title.

---
+++ Write-Title.ps1
@@ -0,0 +1,12 @@
+function Write-Title {
+
+    param(
+        [Parameter(Mandatory = $true)] [String] $Title
+    )
+
+    $line = "********************************************************************************"
+
+    Write-Host $line
+    Write-Host $Title
+    Write-Host $line
+}
