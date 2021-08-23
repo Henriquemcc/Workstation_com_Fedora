0762f208f265bd86d80371398746750fa3a25c33
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-08-23 10:49:14 -0300

    Alterando a verificação de erro do Shell Script para instalar o PowerShell somente quando o comando pwsh resultar em código de retorno 127 (command not found).

--- Executar.sh
+++ Executar.sh
@@ -8,6 +8,8 @@
 } || { # catch
 
   # Caso tenha ocorrido erro, tentando instalar o PowerShell para depois executar o script
-  sudo dnf --assumeyes install https://github.com/PowerShell/PowerShell/releases/download/v7.1.4/powershell-7.1.4-1.centos.8.x86_64.rpm
-  pwsh ./Main.ps1
+  if [ $? -eq 127 ]; then
+    sudo dnf --assumeyes install https://github.com/PowerShell/PowerShell/releases/download/v7.1.4/powershell-7.1.4-1.centos.8.x86_64.rpm
+    pwsh ./Main.ps1
+  fi
 }
