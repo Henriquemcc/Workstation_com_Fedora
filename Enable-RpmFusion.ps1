608acb3d014ef41bb937e4687e03c1e89557e2ba
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-09-18 07:32:47 -0300

    Adicionando os repositórios tainted do RPM Fusion.

--- Enable-RpmFusion.ps1
+++ Enable-RpmFusion.ps1
@@ -12,5 +12,7 @@ function Enable-RpmFusion
 
     Install-DnfPackage -Package @(
     "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$( rpm -E %fedora ).noarch.rpm",
-    "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$( rpm -E %fedora ).noarch.rpm")
+    "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$( rpm -E %fedora ).noarch.rpm",
+    "rpmfusion-free-release-tainted",
+    "rpmfusion-nonfree-release-tainted")
 }
