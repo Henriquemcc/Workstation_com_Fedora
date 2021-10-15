948158f8d98c6796a82989bb0f11d64107d01481
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-10-15 18:23:25 -0300

    Criando o script powershell Install-GitHubCli.ps1.

---
+++ Install-GitHubCli.ps1
@@ -0,0 +1,16 @@
+using module "./Add-DnfRepository.ps1"
+using module "./Install-DnfPackage.ps1"
+
+function Install-GitHubCli {
+
+    <#
+    .SYNOPSIS
+        Installs GitHub Cli.
+    .DESCRIPTION
+        Installs GitHub command line interface.
+    #>
+
+    Add-DnfRepository -Repository "https://cli.github.com/packages/rpm/gh-cli.repo"
+
+    Install-DnfPackage -Package "gh"
+}
