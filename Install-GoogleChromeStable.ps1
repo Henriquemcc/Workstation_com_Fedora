aebd02acb4d34ac18b2a2694dd5e3d8f75f0f71d
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-10-15 18:14:59 -0300

    Criando o script powershell Install-GoogleChromeStable.ps1.

---
+++ Install-GoogleChromeStable.ps1
@@ -0,0 +1,12 @@
+using module "./Install-DnfPackage.ps1"
+
+function Install-GoogleChromeStable {
+    <#
+    .SYNOPSIS
+        Installs Google Chrome.
+    .DESCRIPTION
+        Installs Google Chrome stable version.
+    #>
+
+    Install-DnfPackage -Package "https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm"
+}
