3ab109fad436d7a0a993e61002a0cdfc6cd54574
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-10-15 18:27:23 -0300

    Criando o script powershell Install-MicrosoftTeams.ps1.

---
+++ Install-MicrosoftTeams.ps1
@@ -0,0 +1,16 @@
+using module "./Install-DnfPackage.ps1"
+
+function Install-MicrosoftTeams {
+
+    <#
+    .SYNOPSIS
+        Installs Microsoft Teams.
+    .DESCRIPTION
+        Installs Microsoft Teams.
+    #>
+
+    $urlMicrosoftTeams = (Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/p/?LinkID=2112907&clcid=0x409&culture=en-us&country=US").BaseResponse.RequestMessage.RequestUri.AbsoluteUri
+
+    Install-DnfPackage -Package $urlMicrosoftTeams
+
+}
