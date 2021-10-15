b91fc9c4161114ed967657555d5cd0c6be1e242f
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-10-15 18:39:51 -0300

    Criando o script powershell Install-ProtonVPN.ps1.

---
+++ Install-ProtonVPN.ps1
@@ -0,0 +1,14 @@
+using module "./Install-DnfPackage.ps1"
+
+function Install-ProtonVPN {
+
+    <#
+    .SYNOPSIS
+        Installs Proton VPN.
+    .DESCRIPTION
+        Installs Proton VPN.
+    #>
+
+    Install-DnfPackage -Package @("https://protonvpn.com/download/protonvpn-beta-release-1.0.0-1.noarch.rpm", "protonvpn")
+
+}
