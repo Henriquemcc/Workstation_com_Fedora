712bdcca8cbd2d2c4d2626cde837b47aad5a854b
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-10-15 10:45:47 -0300

    Atualizando o PowerShell.

--- Executar.sh
+++ Executar.sh
@@ -5,6 +5,6 @@ pwsh ./Main.ps1
 
 # Caso tenha ocorrido erro, tentando instalar o PowerShell para depois executar o script
 if [ $? -eq 127 ]; then
-  sudo dnf --assumeyes install https://github.com/PowerShell/PowerShell/releases/download/v7.1.4/powershell-7.1.4-1.centos.8.x86_64.rpm
+  sudo dnf --assumeyes install https://github.com/PowerShell/PowerShell/releases/download/v7.1.5/powershell-7.1.5-1.centos.8.x86_64.rpm
   pwsh ./Main.ps1
 fi
