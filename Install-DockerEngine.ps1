90b2221ae1b2ef6f1669379cf028122e3e667537
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-10-15 11:17:07 -0300

    Criando script PowerShell Install-DockerEngine.ps1.

---
+++ Install-DockerEngine.ps1
@@ -0,0 +1,13 @@
+using module "./Install-DnfPackage.ps1"
+function Install-DockerEngine {
+
+    <#
+    .SYNOPSIS
+        Installs Docker Engine.
+    .DESCRIPTION
+        Installs Docker Engine containerization technology.
+    #>
+    
+    Install-DnfPackage -Package @("curl", "sudo", "bash", "shadow-utils", "coreutils", "systemd")
+    Invoke-Expression -Command "bash Install-DockerEngine.sh"   
+}
