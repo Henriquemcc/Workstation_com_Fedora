b90017e939e87e7f9dff181998a8384948b22ee7
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-10-15 19:18:11 -0300

    Alterando o script powershell Update-SnapPackages.ps1 para que automaticamente instale o snapd caso ele não esteja instalado.

--- Update-SnapPackages.ps1
+++ Update-SnapPackages.ps1
@@ -1,3 +1,5 @@
+using module "./Test-Expression.ps1"
+using module "./Install-Snapd.ps1"
 function Update-SnapPackages
 {
     <#
@@ -6,5 +8,12 @@ function Update-SnapPackages
     .DESCRIPTION
         This function updates all Snap packages to the latest version.
     #>
+
+    # Checking if snapd is installed
+    $snapdIsInstalled = Test-Expression -Command "snap"
+    if (-not $snapdIsInstalled){
+        Install-Snapd
+    }
+
     Invoke-Expression -Command "sudo snap refresh"
 }
