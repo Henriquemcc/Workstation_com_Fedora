c54651066da1cc533d5d52d896f518d4583872e0
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-10-15 10:49:44 -0300

    Criando o shell script Install-PowerShell.sh e o chamando a partir do Executar.sh

--- Executar.sh
+++ Executar.sh
@@ -5,6 +5,6 @@ pwsh ./Main.ps1
 
 # Caso tenha ocorrido erro, tentando instalar o PowerShell para depois executar o script
 if [ $? -eq 127 ]; then
-  sudo dnf --assumeyes install https://github.com/PowerShell/PowerShell/releases/download/v7.1.5/powershell-7.1.5-1.centos.8.x86_64.rpm
+  bash Install-PowerShell.sh
   pwsh ./Main.ps1
 fi
---
+++ Install-PowerShell.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo dnf --assumeyes install https://github.com/PowerShell/PowerShell/releases/download/v7.1.5/powershell-7.1.5-1.centos.8.x86_64.rpm
