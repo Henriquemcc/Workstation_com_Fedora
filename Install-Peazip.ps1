646f3cc7c5f36546d62131a5914dbb8dd4447855
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-10-15 18:31:36 -0300

    Criando o script powershell Install-Peazip.ps1.

---
+++ Install-Peazip.ps1
@@ -0,0 +1,14 @@
+using module "./Install-DnfPackage.ps1"
+
+function Install-Peazip {
+
+    <#
+    .SYNOPSIS
+        Installs Peazip.
+    .DESCRIPTION
+        Installs Peazip.
+    #>
+
+    Install-DnfPackage -Package "https://github.com/peazip/PeaZip/releases/download/8.2.0/peazip-8.2.0.LINUX.GTK2-1.x86_64.rpm"
+
+}
