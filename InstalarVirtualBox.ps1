bb2b02aa4d8983deeb6920434b496ef916d3c335
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-10-18 11:41:17 -0300

    Criando menu para a instalação do VirtualBox.

---
+++ InstalarVirtualBox.ps1
@@ -0,0 +1,71 @@
+using module "./Read-Int32.ps1"
+using module "./MenuOption.psm1"
+using module "./Install-VirtualBox.ps1"
+
+$secureBoot = $false
+$extensionPack = $false
+
+function ObterOpcao
+{
+    $mensagemOpcoes = ""
+    $mensagemOpcoes += "O que deseja fazer?`n"
+    $mensagemOpcoes += "0 - Sair`n"
+    $mensagemOpcoes += "1 - Instalar VirtualBox`n"
+    $mensagemOpcoes += "2 - Compatibilidade com Secure-Boot: "
+
+    if ($secureBoot)
+    {
+        $mensagemOpcoes += "Habilitado"
+    }
+    else
+    {
+        $mensagemOpcoes += "Desabilitado"
+    }
+    $mensagemOpcoes += "`n"
+
+    $mensagemOpcoes += "3 - Pacote de extensão: "
+    if ($extensionPack)
+    {
+        $mensagemOpcoes += "Habilitado"
+    }
+    else
+    {
+        $mensagemOpcoes += "Desabilitado"
+    }
+    $mensagemOpcoes += "`n"
+
+    $opcaoSelecionada = $null
+    while (($null -eq $opcaoSelecionada) -or ($opcaoSelecionada -lt 0) -or ($opcaoSelecionada -gt 3))
+    {
+        $opcaoSelecionada = Read-Int32 -Prompt $mensagemOpcoes
+    }
+
+    return $opcaoSelecionada
+}
+
+function Menu
+{
+    $opcaoSelecionada = $null
+
+    while ($opcaoSelecionada -ne 0)
+    {
+        $opcaoSelecionada = ObterOpcao
+        if ($opcaoSelecionada -ne 0)
+        {
+            if ($opcaoSelecionada -eq 1)
+            {
+                Install-VirtualBox -InstallExtensionPack:$extensionPack -SignLinuxKernelModules:$secureBoot
+            }
+            elseif($opcaoSelecionada -eq 2)
+            {
+                $secureBoot = (-not$secureBoot)
+            }
+            elseif($opcaoSelecionada -eq 3)
+            {
+                $extensionPack = (-not$extensionPack)
+            }
+        }
+    }
+}
+
+Menu
