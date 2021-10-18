588e23ebff09e4b6bf33ef9f92100f1c39e4ad65
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-10-18 07:03:53 -0300

    Documentando e refatorando o script New-AsymmetricPairOfKeysToSignKernelModules.ps1.

--- New-AsymmetricPairOfKeysToSignKernelModules.ps1
+++ New-AsymmetricPairOfKeysToSignKernelModules.ps1
@@ -1,4 +1,5 @@
-using module "./Dnf.psm1"
+using module "./Install-DnfPackage.ps1"
+
 function New-AsymmetricPairOfKeysToSignKernelModules
 {
     <#
@@ -7,23 +8,31 @@ function New-AsymmetricPairOfKeysToSignKernelModules
     .DESCRIPTION
         This function generates a new pair of assimetric key, and add it to the UEFI.
     #>
+
+    # Installing requirements
     Update-DnfPackages
     Install-DnfPackage -Package @("mokutil", "openssl")
 
+    # Creating folder where will be stored modules
     $pathSignedModulesFolder = "/root/signed-modules"
+    $signedModulesFolderExists = Test-Path -Path $pathSignedModulesFolder -Type Container
+    if (-not$signedModulesFolderExists)
+    {
+        New-Item -Path $pathSignedModulesFolder -ItemType Directory -ErrorAction Ignore
+    }
 
-    # Criando a pasta onde será armazenada os módulos
-    New-Item -Path $pathSignedModulesFolder -ItemType Directory -ErrorAction Ignore
+    # Defining the path of public and private keys
+    $pathPrivateKey = Join-Path -Path $pathSignedModulesFolder -ChildPath "MOK.priv"
+    $pathPublicKey = Join-Path -Path $pathSignedModulesFolder -ChildPath "MOK.der"
 
-    $pathMokPriv = Join-Path -Path $pathSignedModulesFolder -ChildPath "MOK.priv"
+    # Creating the pair of keys
+    Invoke-Expression -Command "sudo openssl req -new -x509 -newkey rsa:2048 -keyout $pathPrivateKey -outform DER -out $pathPublicKey -nodes -days 36500 -subj ""/CN=VirtualBox/"""
 
-    $pathMokDer = Join-Path -Path $pathSignedModulesFolder -ChildPath "MOK.der"
+    # Changing private key permissions
+    Invoke-Expression -Command "sudo chmod 600 $pathPrivateKey"
 
-    Invoke-Expression -Command "sudo openssl req -new -x509 -newkey rsa:2048 -keyout $pathMokPriv -outform DER -out $pathMokDer -nodes -days 36500 -subj ""/CN=VirtualBox/"""
+    # Importing public key
+    Invoke-Expression -Command "sudo mokutil --import $pathPublicKey"
 
-    Invoke-Expression -Command "sudo chmod 600 $pathMokPriv"
-
-    Invoke-Expression -Command "sudo mokutil --import $pathMokDer"
-
-    return $pathMokDer, $pathMokPriv
+    return $pathPublicKey, $pathPrivateKey
 }
