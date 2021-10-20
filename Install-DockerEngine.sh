06f53be3b2e5b869d71519ccb0a6d6059ae392a2
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-10-20 12:42:50 -0300

    Corrigindo erro ao instalar o docker em que os comandos finais não eram executados.

--- Install-DockerEngine.sh
+++ Install-DockerEngine.sh
@@ -4,7 +4,6 @@ sudo sh Get-Docker.sh
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
-docker run hello-world
 sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
 sudo chmod g+rwx "$HOME/.docker" -R
 sudo systemctl enable docker.service docker.socket containerd.service
