0cfb760605403c0ac1cf74016b4c86964ae441e1
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-11-10 12:50:14 -0300

    Atualizando o PowerShell.

--- Install-PowerShell.sh
+++ Install-PowerShell.sh
@@ -2,14 +2,14 @@
 
 function InstallPowerShell_On_Fedora_x86_64
 {
-  sudo dnf --assumeyes install https://github.com/PowerShell/PowerShell/releases/download/v7.1.5/powershell-7.1.5-1.centos.8.x86_64.rpm
+  sudo dnf --assumeyes install https://github.com/PowerShell/PowerShell/releases/download/v7.2.0/powershell-lts-7.2.0-1.rh.x86_64.rpm
 }
 
 function InstallPowerShell_On_Fedora_aarch64
 {
-  wget --no-clobber https://github.com/PowerShell/PowerShell/releases/download/v7.1.5/powershell-7.1.5-linux-arm64.tar.gz
+  wget --no-clobber https://github.com/PowerShell/PowerShell/releases/download/v7.2.0/powershell-7.2.0-linux-arm64.tar.gz
   sudo mkdir /usr/share/powershell
-  sudo tar -xvf ./powershell-7.1.5-linux-arm64.tar.gz --directory /usr/share/powershell/
+  sudo tar -xvf ./powershell-7.2.0-linux-arm64.tar.gz --directory /usr/share/powershell/
   sudo ln --symbolic /usr/share/powershell/pwsh /bin/pwsh
   sudo chmod 777 /bin/pwsh
 }
