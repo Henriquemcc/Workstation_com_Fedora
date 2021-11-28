f3f72d166d9521c18b52b260efcb3eea004b335e
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-11-28 18:46:05 -0300

    Adding bash script Set-GnomeShellSettings.sh

---
+++ Set-GnomeShellSettings.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Desabilitando hot corners
+gsettings set org.gnome.desktop.interface enable-hot-corners false
+
+# Mostrar segundos
+gsettings set org.gnome.desktop.interface clock-show-seconds true
+
+# Mostrar dia da semana
+gsettings set org.gnome.desktop.interface clock-show-weekday true
+
+# Mostrar porcentagem da bateria
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+
+# Habilitar atualização de fuso horário
+gsettings set org.gnome.desktop.datetime automatic-timezone true
+
+# Desabilitar autorun
+gsettings set org.gnome.desktop.media-handling autorun-never true
+
+# Habilitando botões minimizar e maximizar
+gsettings set org.gnome.desktop.wm.preferences button-layout "appmenu:minimize,maximize,close"
+
+# Habilitando icones na área de trabalho
+gsettings set org.gnome.desktop.background show-desktop-icons true
+
+# Habilitando localização automatica gnome shell weather
+gsettings set org.gnome.shell.weather automatic-location true
+
+# Habilitando o Fractional scaling
+gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"
