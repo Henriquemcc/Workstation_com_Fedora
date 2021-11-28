94fa7741b7ef26c20e38013d0b3f5f6d4a25b84d
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-11-28 18:48:13 -0300

    Adding the installation of requirements.

--- Install-GithubCli.sh
+++ Install-GithubCli.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# Installing requirements
+sudo dnf install git --assumeyes
+sudo dnf install git-lfs --assumeyes
+
+# Adding GitHub Cli repository
 sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo --assumeyes
+
+# Installing GitHub Cli.
 sudo dnf install gh --assumeyes
