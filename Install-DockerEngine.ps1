672852f98372b63cafccee05eeaa171368838552
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-12-09 12:08:08 -0300

    Replacing Install-DockerEngine.sh with Install-Docker.sh

--- Install-DockerEngine.ps1
+++ Install-DockerEngine.ps1
@@ -6,4 +6,4 @@
 #>
 
 ./Install-DnfPackage.ps1 -Package @("curl", "sudo", "bash", "shadow-utils", "coreutils", "systemd", "fuse-overlayfs", "iptables", "docker-compose")
-Invoke-Expression -Command "bash Install-DockerEngine.sh"
+Invoke-Expression -Command "bash Install-Docker.sh"
