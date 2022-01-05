c02d665365be1ba60f7dcb125016e598f279c2eb
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-01-05 07:57:23 -0300

    Fixing Install-PowerShell.sh

---
+++ Install-PowerShell.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# Defining Urls
+url_x86_64_rpm="https://github.com/PowerShell/PowerShell/releases/download/v7.2.1/powershell-lts-7.2.1-1.rh.x86_64.rpm"
+url_arm64_tar_gz="https://github.com/PowerShell/PowerShell/releases/download/v7.2.1/powershell-7.2.1-linux-arm64.tar.gz"
+url_arm32_tar_gz="https://github.com/PowerShell/PowerShell/releases/download/v7.2.1/powershell-7.2.1-linux-arm32.tar.gz"
+
+# Downloads a file from a url
+# @param $1 File url
+# @return Downloaded file path
+function Download_File
+{
+  # Defining temporary directory
+  temp_dir="/tmp"
+
+  # Creating temporary directory if it does not exist
+  if ! [ -d $temp_dir ]; then
+      mkdir -p $temp_dir > /dev/null 2>&1
+  fi
+
+  # Defining file name
+  file_name="$(basename "$1")"
+
+  # Defining file path
+  file_path="${temp_dir}/${file_name}"
+
+  # Downloading file
+  curl -L "$1" --output "$file_path" > /dev/null 2>&1
+
+  # Returning file path
+  echo "$file_path"
+}
+
+function InstallPowerShell_On_Fedora_x86_64
+{
+  sudo dnf --assumeyes install $url_x86_64_rpm
+}
+
+function InstallPowerShell_On_Fedora_arm
+{
+  # Downloading file
+  downloaded_file="$(Download_File "$1")"
+
+  # Defining installation directory
+  installation_directory="/usr/share/powershell"
+
+  # Creating installation directory if it does not exist
+  if ! [ -d $installation_directory ]; then
+      sudo mkdir -p $installation_directory
+  fi
+
+  # Extracting downloaded file into installation directory
+  sudo tar -xvf "$downloaded_file" --directory $installation_directory
+
+  # Creating symbolic link
+  sudo ln --symbolic /usr/share/powershell/pwsh /bin/pwsh
+
+  # Changing file permission
+  sudo chmod 777 /bin/pwsh
+}
+
+function InstallPowerShell_On_Fedora_arm32
+{
+    InstallPowerShell_On_Fedora_arm $url_arm32_tar_gz
+}
+
+function InstallPowerShell_On_Fedora_arm64
+{
+    InstallPowerShell_On_Fedora_arm $url_arm64_tar_gz
+}
+
+# x86_64
+if [ "$(uname -m)" == "x86_64" ]; then
+  InstallPowerShell_On_Fedora_x86_64
+
+# Arm
+elif [[ "$(uname -m)" == "aarch"* ]] || [[ "$(uname -m)" == "arm"* ]] ; then
+
+    # 64 bits
+    if [ "$(uname -m)" == "aarch64" ] || [ "$(uname -m)" == "arm64" ] || { [ "$(command -v getconf)" ] && [ "$(getconf LONG_BIT)" == 64 ]; } ; then
+      InstallPowerShell_On_Fedora_arm64
+    # 32 bits
+    else
+      InstallPowerShell_On_Fedora_arm32
+    fi
+fi
