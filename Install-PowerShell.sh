ce513a23f9ca9dfeb78ba27b9502c59e87dd1471
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-01-05 06:41:17 -0300

    Updating PowerShell from 7.1.5 to 7.2.1

--- Install-PowerShell.sh
+++ Install-PowerShell.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo dnf --assumeyes install https://github.com/PowerShell/PowerShell/releases/download/v7.1.5/powershell-7.1.5-1.centos.8.x86_64.rpm
+sudo dnf --assumeyes install https://github.com/PowerShell/PowerShell/releases/download/v7.2.1/powershell-lts-7.2.1-1.rh.x86_64.rpm
