3685f27e0f398a1eb5e33418b3b6cab0a7356f90
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-01-05 17:08:43 -0300

    Adding shell script Install-Minecraft.sh

---
+++ Install-Minecraft.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+minecraft_file_url="https://launcher.mojang.com/download/Minecraft.tar.gz"
+temporary_directory="/tmp"
+installation_directory="${HOME}/.bin"
+minecraft_binary_file_name="minecraft-launcher"
+applications_folder="${HOME}/.local/share/applications"
+minecraft_picture_url="https://www.minecraft.net/etc.clientlibs/minecraft/clientlibs/main/resources/img/GrassBlock_HighRes.png"
+icons_directory="${HOME}/.local/share/icons/hicolor/303x303/apps"
+
+# Downloads a file from the internet.
+# @param $1 File url
+# @return Downloaded file path
+function download_file
+{
+  # Defining file name
+  file_name="$(basename "$1")"
+
+  # Defining destination path
+  file_path="${temporary_directory}/${file_name}"
+
+  # Erasing file if it exists
+  if [ -f "$file_path" ]; then
+      rm "$file_path"
+  fi
+
+  # Downloading file
+  curl -L "$1" --output "$file_path"
+
+  # Returning file path
+  echo "$file_path"
+}
+
+# Creates a shortcut.
+# @param $1 Executable path
+# @param $2 Icon path
+# @param $3 Shortcut path
+function create_shortcut
+{
+  {
+    echo "[Desktop Entry]"
+    echo "Version=1.5"
+    echo "Name=Minecraft"
+    echo "Type=Application"
+    echo "Exec=$1"
+    echo "Icon=$2"
+    echo "StartupNotify=true"
+    echo "Categories=Game;"
+  } > "$3"
+
+}
+
+# Downloading file
+downloaded_tar_gz_file="$(download_file $minecraft_file_url)"
+
+# Extracting Minecraft binary
+tar -C "$temporary_directory" -xzvf "$downloaded_tar_gz_file"
+
+# Moving binary file to ~/.bin
+extracted_minecraft_binary_path="${temporary_directory}/minecraft-launcher/${minecraft_binary_file_name}"
+minecraft_binary_path="${installation_directory}/minecraft-launcher"
+mv "$extracted_minecraft_binary_path" "$minecraft_binary_path"
+
+# Creating icon directory if it does not exist
+if ! [ -d "$icons_directory" ]; then
+    mkdir -p "$icons_directory"
+fi
+
+# Defining Minecraft icon path
+minecraft_icon_path="${icons_directory}/minecraft.png"
+
+# Downloading icon
+curl -L --user-agent "Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0" "$minecraft_picture_url" > "$minecraft_icon_path"
+
+# Defining Minecraft shortcut file path
+minecraft_shortcut_path="${applications_folder}/Minecraft.desktop"
+
+# Creating shortcut
+create_shortcut "$minecraft_binary_path" "$minecraft_icon_path" "$minecraft_shortcut_path"
