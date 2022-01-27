89a75bb9685e2941cc52926e335c957c95be5681
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-01-27 09:00:16 -0300

    Adding the check if sdk command exists

--- Install-SdkMan.sh
+++ Install-SdkMan.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo dnf install --assumeyes curl
-curl -s "https://get.sdkman.io" | bash
-source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk version
+if ! [ "$(command -v sdk)" ]; then
+  sudo dnf install --assumeyes curl
+  curl -s "https://get.sdkman.io" | bash
+  source "$HOME/.sdkman/bin/sdkman-init.sh"
+  sdk version
+fi
