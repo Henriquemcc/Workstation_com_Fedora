c18be86fab926e9b2a1ebe4c3cb6987d436691c3
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-01-27 09:43:12 -0300

    Adding the installation of requirements

--- Install-RustLang.sh
+++ Install-RustLang.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+# Installing requirements
+sudo dnf install --assumeyes curl
+
+# Installing RustLang
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Running RustLang environment
 source $HOME/.cargo/env
