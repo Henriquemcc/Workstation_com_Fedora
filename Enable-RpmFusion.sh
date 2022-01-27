00b4955730f3f13847ca5886a415ffd9c14f4a88
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-01-27 08:24:08 -0300

    Formatting script

--- Enable-RpmFusion.sh
+++ Enable-RpmFusion.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm --assumeyes
-sudo dnf install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm --assumeyes
-sudo dnf install rpmfusion-free-release-tainted --assumeyes
-sudo dnf install rpmfusion-nonfree-release-tainted --assumeyes
+sudo dnf install --assumeyes https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install --assumeyes https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install --assumeyes rpmfusion-free-release-tainted
+sudo dnf install --assumeyes rpmfusion-nonfree-release-tainted
