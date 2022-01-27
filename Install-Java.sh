5fe219a1b673749b3adbbbd6e5a6fa23471c5e86
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-01-27 10:27:31 -0300

    Adding JDK installation

--- Install-Java.sh
+++ Install-Java.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 if ! [ "$(command -v java)" ]; then
-    sudo dnf install --assumeyes java-1.8.0-openjdk.x86_64
+    sudo dnf install --assumeyes java-1.8.0-openjdk
+    sudo dnf install --assumeyes sudo dnf install java-1.8.0-openjdk-devel
 fi
