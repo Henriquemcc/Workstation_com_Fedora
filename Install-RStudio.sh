cd0a347e52fb458b43e64ca7ce1248d9b228f7ce
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-01-27 09:39:40 -0300

    Adding script

---
+++ Install-RStudio.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Installing requirements
+sudo dnf install --assumeyes R
+
+# Installing R Studio
+sudo dnf install --assumeyes https://download1.rstudio.org/desktop/centos8/x86_64/rstudio-1.4.1717-x86_64.rpm
+
+
