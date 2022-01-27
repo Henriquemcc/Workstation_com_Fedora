e914c2364ab58ef7b8a62d5f39a2bdef406822b8
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-01-27 09:45:05 -0300

    Adding the installation of requirements

--- Install-VirtualBox.sh
+++ Install-VirtualBox.sh
@@ -3,5 +3,8 @@
 # Installing VirtualBox
 sudo dnf install --assumeyes virtualbox
 
+# Installing requirements
+bash ./Install-PowerShell.sh
+
 # Siging VirtualBox Kernel Modules
 sudo pwsh ./Install-VirtualBoxSignedKernelModules.ps1
