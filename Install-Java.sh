2df10d1d9ccd1289af4d87ccade56af2da36d95a
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-01-27 10:28:11 -0300

    Fixing script

--- Install-Java.sh
+++ Install-Java.sh
@@ -2,5 +2,5 @@
 
 if ! [ "$(command -v java)" ]; then
     sudo dnf install --assumeyes java-1.8.0-openjdk
-    sudo dnf install --assumeyes sudo dnf install java-1.8.0-openjdk-devel
+    sudo dnf install --assumeyes java-1.8.0-openjdk-devel
 fi
