199551f20c0b1312f18bdbd633e867687cd79e61
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-01-27 08:30:44 -0300

    Adding requirements and docker compose

--- Install-Docker.sh
+++ Install-Docker.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+sudo dnf install --assumeyes shadow-utils
+sudo dnf install --assumeyes coreutils
+sudo dnf install --assumeyes systemd
+sudo dnf install --assumeyes fuse-overlayfs
+sudo dnf install --assumeyes iptables
+
 curl -fsSL https://get.docker.com -o get-docker.sh
 DRY_RUN=1 sh ./get-docker.sh
+
+sudo dnf install --assumeyes docker-compose
