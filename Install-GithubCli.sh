07a0a55f1fac2db23e71334b646444acbaa60576
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-01-27 08:47:57 -0300

    Formatting script

--- Install-GithubCli.sh
+++ Install-GithubCli.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Installing requirements
-sudo dnf install git --assumeyes
-sudo dnf install git-lfs --assumeyes
+sudo dnf install --assumeyes git
+sudo dnf install --assumeyes git-lfs
 
 # Adding GitHub Cli repository
 sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo --assumeyes
 
 # Installing GitHub Cli.
-sudo dnf install gh --assumeyes
+sudo dnf install --assumeyes gh
