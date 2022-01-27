09fb34f1f3860dd736331f2b23c8b028305d4211
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-01-27 09:53:09 -0300

    Removing unused PowerShell scripts

--- Update-DnfPackages.ps1
+++
@@ -1,8 +0,0 @@
-<#
-.SYNOPSIS
-    Updates all DNF Packages to the latest version.
-.DESCRIPTION
-    This updates all DNF Packages to the latest version.
-#>
-
-Invoke-Expression -Command "sudo dnf --assumeyes upgrade --refresh"
