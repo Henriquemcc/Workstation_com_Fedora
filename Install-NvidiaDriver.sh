7b27ab26ce78d99fd852a3de8e2398223e71b1e3
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-02-16 20:12:19 -0300

    Adding the function to sign Nvidia kernel modules

--- Install-NvidiaDriver.sh
+++ Install-NvidiaDriver.sh
@@ -1,10 +1,44 @@
 #!/bin/bash
 
+function sign_nvidia_kernel_modules() {
+
+  if [ "$(mokutil --sb-state)" == "SecureBoot enabled" ]; then
+
+    # Installing requirements
+    sudo dnf install --assumeyes kmod
+    sudo dnf install --assumeyes coreutils
+
+    # Setting private and public key path
+    path_folder_signed_modules="/root/signed-modules"
+    path_private_key="$path_folder_signed_modules/private_key.priv"
+    path_public_key="$path_folder_signed_modules/public_key.der"
+
+    # Checking if private and public keys file exist
+    bash ./New-KernelModulesPairOfKeys.sh
+
+    # Getting information about Nvidia module for Linux kernel
+    nvidia="$(sudo modinfo -n nvidia)"
+    nvidia_parent_folder="$(dirname "$nvidia")"
+
+    # Getting the location of sign-file executable
+    uname_str="$(uname -r)"
+    sign_file_path="/usr/src/kernels/$uname_str/scripts/sign-file"
+
+    # Signing kernel modules
+    for file in "$nvidia_parent_folder"/* ; do
+      if [[ "$file" == *.ko ]]; then
+        eval "sudo $sign_file_path" sha256 "$path_private_key" "$path_public_key" "$file"
+      fi
+    done
+  fi
+}
+
 # Enabling RPM Fusion
-if [ "$(dnf repolist --repoid rpmfusion-nonfree | sed -n 2p)" != "Error: Unknown repo: 'rpmfusion-nonfree'" ]; then
-    bash Enable-RpmFusion.sh
-fi
+bash ./Enable-RpmFusion.sh
 
 # Installing Nvidia
-sudo dnf install akmod-nvidia --assumeyes
-sudo dnf install xorg-x11-drv-nvidia-cuda --assumeyes
+sudo dnf install --assumeyes akmod-nvidia
+sudo dnf install --assumeyes xorg-x11-drv-nvidia-cuda
+
+# Signing kernel modules
+sign_nvidia_kernel_modules
