983001214d5c3c64d1d151942c2fdf4ec7a2f4b8
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-02-16 20:10:16 -0300

    Adding the check for existing files, adding the check for secure boot enabled and adding function run_as_root

--- New-KernelModulesPairOfKeys.sh
+++ New-KernelModulesPairOfKeys.sh
@@ -1,40 +1,67 @@
 #!/bin/bash
 
+# Runs this script as root if it is not root.
+function run_as_root() {
+  if [ "$(whoami)" != "root" ]; then
+    echo "This script is not running as root"
+    echo "Elevating privileges..."
+    if [ "$(command -v sudo)" ]; then
+      sudo bash "$0"
+      exit $?
+    else
+      echo "Sudo is not installed"
+      exit 1
+    fi
+  fi
+}
+
+# Checking if secure boot is enabled
+if [ "$(mokutil --sb-state)" != "SecureBoot enabled" ]; then
+  exit 0
+fi
+
+# Running as root
+run_as_root
+
 # Creating signed modules folder
 path_folder_signed_modules="/root/signed-modules"
-sudo mkdir -p "$path_folder_signed_modules"
+mkdir -p "$path_folder_signed_modules"
 
 # Creating configuration file
 path_configuration_file="$path_folder_signed_modules/configuration_file.config"
+
+# Setting key files path
+path_private_key="$path_folder_signed_modules/private_key.priv"
+path_public_key="$path_folder_signed_modules/public_key.der"
+
+# Exiting if files exist
+if [[ -f "$path_private_key" && -f "$path_public_key" ]]; then
+  echo "File already exist"
+  exit 0
+fi
+
 {
   echo "[ req ]"
   echo "default_bits = 4096"
-  #echo "distinguished_name = req_distinguished_name"
+  echo "distinguished_name = req_distinguished_name"
   echo "prompt = no"
   echo "string_mask = utf8only"
   echo "x509_extensions = myexts"
-  #echo
-  #echo "[ req_distinguished_name ]"
-  #echo "O = Organization"
-  #echo "CN = Organization signing key"
-  #echo "emailAddress = E-mail address"
+  echo
+  echo "[ req_distinguished_name ]"
+  echo "O = Organization"
+  echo "CN = Organization signing key"
+  echo "emailAddress = E-mail address"
   echo
   echo "[ myexts ]"
   echo "basicConstraints=critical,CA:FALSE"
   echo "keyUsage=digitalSignature"
   echo "subjectKeyIdentifier=hash"
   echo "authorityKeyIdentifier=keyid"
-} | sudo tee "$path_configuration_file"
-
-# Setting key files path
-path_private_key="$path_folder_signed_modules/private_key.priv"
-path_public_key="$path_folder_signed_modules/public_key.der"
+} >"$path_configuration_file"
 
 # Creating pair of keys
-sudo openssl req -x509 -new -nodes -utf8 -sha256 -days 36500 -batch -config "$path_configuration_file" -outform DER -out "$path_public_key" -keyout "$path_private_key"
+openssl req -x509 -new -nodes -utf8 -sha256 -days 36500 -batch -config "$path_configuration_file" -outform DER -out "$path_public_key" -keyout "$path_private_key"
 
 # Adding keys to UEFI
-sudo mokutil --import "$path_public_key"
-
-# Returning private key file path, public key file path, and folder path
-echo "$path_private_key;$path_public_key;$path_folder_signed_modules"
+mokutil --import "$path_public_key"
