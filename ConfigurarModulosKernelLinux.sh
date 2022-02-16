d9d7a0ff900da0d818397fd21fab1d10be632231
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-02-16 20:22:41 -0300

    Adicionando script

---
+++ ConfigurarModulosKernelLinux.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+function_return_variable=
+
+function obter_opcao() {
+  _opcao_selecionada=-1
+  re='^[0-9]+$'
+  while ! [[ $_opcao_selecionada =~ $re ]] || [ $_opcao_selecionada -lt 0 ] || [ $_opcao_selecionada -gt 2 ]; do
+    echo "O que deseja fazer?"
+    echo "0 - Sair"
+    echo "1 - Adicionar módulos do driver da NVIDIA ao Kernel Linux"
+    echo "2 - Adicionar módulos do VirtualBox ao Kernel Linux"
+    read -r _opcao_selecionada
+  done
+
+  function_return_variable="$_opcao_selecionada"
+}
+
+opcao_selecionada=-1
+while [ $opcao_selecionada -ne 0 ]; do
+
+  # Obtendo opção selecionada pelo usuário
+  obter_opcao
+  opcao_selecionada="$function_return_variable"
+
+  if [ "$opcao_selecionada" -eq 1 ]; then
+    bash ./Install-NvidiaDriver.sh
+  elif [ "$opcao_selecionada" -eq 2 ]; then
+    bash ./Install-VirtualBox.sh
+  fi
+done
+
+
