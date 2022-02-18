092c1d9dbed442641e946b0a2f5a45c70933c691
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-02-18 12:01:12 -0300

    Adding script

---
+++ Install-TechnicLauncher.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+url_technic_launcher_file="https://launcher.technicpack.net/launcher4/709/TechnicLauncher.jar"
+url_technic_launcher_picture="https://i.imgur.com/MDyTikO.png"
+path_installation_directory="${HOME}/.bin"
+path_applications_folder="${HOME}/.local/share/applications"
+path_icons_directory="${HOME}/.local/share/icons/hicolor/303x303/apps"
+
+# Installing requirements
+bash ./Install-Java8.sh
+
+# Downloading jar file
+path_technic_launcher_jar="${path_installation_directory}/TechnicLauncher.jar"
+curl -L "$url_technic_launcher_file" > "$path_technic_launcher_jar"
+
+# Creating icon directory if it does not exist
+mkdir -p "$path_icons_directory"
+
+# Downloading icon file
+path_technic_launcher_icon="${path_icons_directory}/technic_launcher.png"
+curl -L "$url_technic_launcher_picture" > "$path_technic_launcher_icon"
+
+# Creating shortcut
+path_technic_launcher_shortcut="${path_applications_folder}/TechnicLauncher.desktop"
+{
+    echo "[Desktop Entry]"
+    echo "Version=1.5"
+    echo "Name=TechnicLauncher"
+    echo "Type=Application"
+    echo "Exec=java8 -jar $path_technic_launcher_jar"
+    echo "Icon=$path_technic_launcher_icon"
+    echo "StartupNotify=true"
+    echo "Categories=Game;"
+  } > "$path_technic_launcher_shortcut"
