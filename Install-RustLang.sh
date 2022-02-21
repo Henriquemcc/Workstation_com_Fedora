c187f981cfe56e2ab688b8d7b8e90744bb86cdec
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-02-21 14:10:59 -0300

    Changing to silent install

--- Install-RustLang.sh
+++ Install-RustLang.sh
@@ -4,7 +4,8 @@
 sudo dnf install --assumeyes curl
 
 # Installing RustLang
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > rustup-init.sh
+sh ./rustup-init.sh --quiet --component cargo rustc rustup --profile complete -y
 
 # Running RustLang environment
 source $HOME/.cargo/env
