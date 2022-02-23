7a3cd041dad84e95ec46cf8906c42d7e24c80a06
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-02-23 15:40:58 -0300

    Adding the check if it is already installed

--- Install-SpeedTestCli.sh
+++ Install-SpeedTestCli.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-curl -s https://install.speedtest.net/app/cli/install.rpm.sh | sudo bash
-sudo dnf --assumeyes install speedtest
+if ! [ "$(command -v speedtest)" ]; then
+  curl -s https://install.speedtest.net/app/cli/install.rpm.sh | sudo bash
+  sudo dnf --assumeyes install speedtest
+fi
