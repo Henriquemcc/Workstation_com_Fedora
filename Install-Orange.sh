e3e2ed94e3c2b3e2f2b351bc4f19c8491202ab23
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-02-23 21:05:16 -0300

    Adding script

---
+++ Install-Orange.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Installing library Python.h
+if ! dnf list --installed python3-devel > /dev/null; then
+  sudo dnf install --assumeyes python3-devel
+fi
+
+# Installing Python Pip
+if ! [ "$(command -v pip)" ]; then
+  sudo dnf install --assumeyes python3-pip
+fi
+
+# Installing orange
+if ! [ "$(command -v orange-canvas)" ]; then
+  pip install orange3
+fi
+
+# Getting orange installation dir
+path_orange_installation_dir="$(pip show orange3 | grep "Location: ")"
+path_orange_installation_dir="${path_orange_installation_dir/Location: /""}"
+path_orange_installation_dir="${path_orange_installation_dir}/Orange"
+
+# Getting orange icon
+path_orange_icon="${path_orange_installation_dir}/canvas/icons/orange-canvas.svg"
+
+# Creating shortcut
+path_shortcut="${HOME}/.local/share/applications/Orange.desktop"
+{
+  echo "[Desktop Entry]"
+  echo "Version=1.5"
+  echo "Name=Orange"
+  echo "Type=Application"
+  echo "Exec=orange-canvas"
+  echo "Icon=$path_orange_icon"
+  echo "StartupNotify=true"
+  echo "Categories=Education;Science;"
+} >"$path_shortcut"
