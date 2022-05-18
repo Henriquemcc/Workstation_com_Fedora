8ba0be5ef0b307cc559d9580a0c3e79b39159af9
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-05-18 10:09:39 -0300

    - Renaming script 'Install-Docker.sh' to 'Install-DockerEngine.bash'. - Simplifying installation script.

---
+++ Install-DockerEngine.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Downloading convenience script
+curl -fsSL https://get.docker.com -o get-docker.sh
+
+# Running convenience script
+sudo sh get-docker.sh
+
+# Installing docker rootless
+dockerd-rootless-setuptool.sh install
+
+# Enabling docker service
+sudo systemctl enable docker.service
