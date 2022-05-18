0e8feffcd8cb867f2568e84578c95e1f8d093f25
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-05-18 10:46:12 -0300

    - Adicionando referência para o script de instalação do HMCC Server.

--- Instalar.bash
+++ Instalar.bash
@@ -5,10 +5,11 @@ function_return_variable=
 function obter_opcao() {
   _opcao_selecionada=-1
   re='^[0-9]+$'
-  while ! [[ $_opcao_selecionada =~ $re ]] || [ $_opcao_selecionada -lt 0 ] || [ $_opcao_selecionada -gt 1 ]; do
+  while ! [[ $_opcao_selecionada =~ $re ]] || [ $_opcao_selecionada -lt 0 ] || [ $_opcao_selecionada -gt 2 ]; do
     echo "O que deseja fazer?"
     echo "0 - Sair"
     echo "1 - Instalar sistema Henrique-PC"
+    echo "2 - Instalar sistema HMCC-Server"
     read -r _opcao_selecionada
   done
 
@@ -24,5 +25,7 @@ while [ $opcao_selecionada -ne 0 ]; do
 
   if [ "$opcao_selecionada" -eq 1 ]; then
     bash ./Instalar_Henrique_PC.bash
+  elif [ "$opcao_selecionada" -eq 2 ]; then
+    bash ./Instalar_HMCC_Server.bash
   fi
 done
