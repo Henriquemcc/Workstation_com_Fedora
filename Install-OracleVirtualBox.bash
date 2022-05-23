fad573427cf13c7be94bb99a40a121fffdf6be2b
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-05-23 19:49:03 -0300

    Improving Oracle VirtualBox installation script.

--- Install-OracleVirtualBox.bash
+++ Install-OracleVirtualBox.bash
@@ -1,3 +1,17 @@
 #!/bin/bash
-sudo dnf install --assumeyes https://download.virtualbox.org/virtualbox/6.1.34/VirtualBox-6.1-6.1.34_150636_fedora36-2.x86_64.rpm
+
+# Removing VirtualBox packages from other repositories
+sudo dnf autoremove --assumeyes --repoid rpmfusion-free VirtualBox VirtualBox-kmodsrc VirtualBox-server akmod-VirtualBox kmod-VirtualBox
+
+# Downloading and registering Oracle VirtualBox Key
+wget https://www.virtualbox.org/download/oracle_vbox.asc
+sudo rpm --import ./oracle_vbox.asc
+gio trash ./oracle_vbox.asc
+
+# Adding Oracle VirtualBox Repository
+wget "https://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo"
+sudo mv ./virtualbox.repo /etc/yum.repos.d/virtualbox.repo
+
+# Installing VirtualBox
+sudo dnf install --assumeyes --repoid virtualbox VirtualBox-6.1
 sudo /sbin/vboxconfig
