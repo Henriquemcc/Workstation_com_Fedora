0e38a8ce578dc5b44266140705f39a6c40305871
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-05-26 10:23:07 -0300

    Replacing DNF package with Flatpak package.

--- Install-Peazip.bash
+++ Install-Peazip.bash
@@ -1,2 +1,7 @@
 #!/bin/bash
-sudo dnf install --assumeyes https://github.com/peazip/PeaZip/releases/download/8.2.0/peazip-8.2.0.LINUX.GTK2-1.x86_64.rpm
+
+# Installing dependencies
+bash ./Install-Flathub.bash
+
+# Installing PeaZip
+flatpak install flathub io.github.peazip.PeaZip
