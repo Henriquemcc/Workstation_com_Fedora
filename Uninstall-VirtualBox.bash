7b1bddeaa59220881d89f3cb1a3210f76f5e0fac
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-06-07 17:30:49 -0300

    Adding script

---
+++ Uninstall-VirtualBox.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Removing RPM Fusion's VirtualBox
+sudo dnf autoremove --assumeyes --repoid rpmfusion-free VirtualBox VirtualBox-kmodsrc VirtualBox-server akmod-VirtualBox kmod-VirtualBox
