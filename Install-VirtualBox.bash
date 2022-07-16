3d03fd224d246b64ae641990f42e8cda896f365c
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-07-16 18:23:55 -0300

    Integrating scripts Sign-VirtualBox.bash and Install-VirtualBox.bash

--- Install-VirtualBox.bash
+++ Install-VirtualBox.bash
@@ -10,50 +10,16 @@ function sign_virtualbox_kernel_modules() {
     sudo dnf install --assumeyes akmod-VirtualBox
     sudo dnf install --assumeyes kernel-devel
     sudo dnf install --assumeyes kernel-devel-"$(uname -r)"
+    sudo dnf install --assumeyes kernel-headers
     sudo dnf install --assumeyes akmods
     sudo dnf install --assumeyes kmod-VirtualBox
+    sudo dnf install --assumeyes dkms
+    sudo dnf install --assumeyes qt5-qtx11extras
+    sudo dnf install --assumeyes elfutils-libelf-devel
+    sudo dnf install --assumeyes zlib-devel
+    sudo dnf install --assumeyes @development-tools
 
-    # Setting private and public key path
-    path_folder_signed_modules="/root/signed-modules"
-    path_private_key="$path_folder_signed_modules/private_key.priv"
-    path_public_key="$path_folder_signed_modules/public_key.der"
-
-    # Checking if private and public keys file exist
-    bash ./New-KernelModulesPairOfKeys.bash
-
-    # VirtualBox module names
-    declare -a module_names
-    module_names+=("vboxdrv")
-    module_names+=("vboxnetflt")
-    module_names+=("vboxnetadp")
-    module_names+=("vboxpci")
-
-    for module_name in "${module_names[@]}"; do
-
-      # Getting information about VirtualBox module for Linux kernel
-      module_file_name="$(modinfo -n "$module_name")"
-      module_file_parent_folder="$(dirname "$module_file_name")"
-
-      # Getting the location of sign-file executable
-      sign_file_path="/usr/src/kernels/$(uname -r)/scripts/sign-file"
-
-      # Signing kernel modules
-      for file in "$module_file_parent_folder"/*; do
-        if [[ "$file" == *.xz ]]; then
-          xz --decompress "$file"
-          file=${file::3}
-        fi
-        if [[ "$file" == *.ko ]]; then
-          eval "sudo $sign_file_path" sha256 "$path_private_key" "$path_public_key" "$file"
-        fi
-      done
-    done
-
-    # Loading kernel modules
-    sudo akmods
-
-    # Restarting VirtualBox service
-    sudo systemctl restart vboxdrv.service
+    bash ./Sign-VirtualBox.bash
 
   fi
 }
