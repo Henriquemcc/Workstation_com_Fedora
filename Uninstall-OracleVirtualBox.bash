188cf85a5e445b02934938ec642cd71da5d0c33e
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-07-16 18:31:57 -0300

    Fixing uninstall scripts

--- Uninstall-OracleVirtualBox.bash
+++ Uninstall-OracleVirtualBox.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Removing VirtualBox package
-sudo dnf autoremove --assumeyes --repoid virtualbox VirtualBox-6.1
+sudo dnf autoremove --assumeyes virtualbox VirtualBox-6.1
 
 # Removing VirtualBox repository
 sudo rm /etc/yum.repos.d/virtualbox.repo
