b2d88d1dec84877df5e2093eb4efeae0f57214a8
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-07-17 13:16:55 -0300

    Removing commented code.

--- Sign-VirtualBox.bash
+++ Sign-VirtualBox.bash
@@ -18,13 +18,6 @@ function run_as_root() {
 function sign_virtualbox_modules() {
   if [ "$(mokutil --sb-state)" == "SecureBoot enabled" ]; then
 
-    # Clean module files before signing; For debug only
-    #sudo dnf remove --assumeyes VirtualBox-kmodsrc VirtualBox-server akmod-VirtualBox kernel-devel-$(uname -r) kmod-VirtualBox virtualbox-guest-additions VirtualBox
-    #sudo dnf remove --assumeyes kmod-VirtualBox-6.1.34-3.fc36.x86_64
-    #sudo dnf install --assumeyes VirtualBox-kmodsrc VirtualBox-server akmod-VirtualBox kernel-devel-$(uname -r) kmod-VirtualBox virtualbox-guest-additions VirtualBox kmod-VirtualBox-6.1.34-3.fc36.x86_64
-    #sudo dnf install --assumeyes kmod-VirtualBox-6.1.34-3.fc36.x86_64
-    #sudo dnf reinstall --assumeyes kmod-VirtualBox-6.1.34-3.fc36.x86_64
-
     # Checking if private and public keys file exist
     bash ./New-KernelModulesPairOfKeys.bash
 
@@ -32,15 +25,10 @@ function sign_virtualbox_modules() {
     path_folder_signed_modules="/root/signed-modules"
     path_private_key="$path_folder_signed_modules/private_key.priv"
     path_public_key="$path_folder_signed_modules/public_key.der"
-    #echo "DEBUG1: \$path_folder_signed_modules = $path_folder_signed_modules"
-    #echo "DEBUG2: \$path_private_key = $path_private_key"
-    #echo "DEBUG3: \$path_public_key = $path_public_key"
 
     sign_file_binary_path="/usr/src/kernels/$(uname -r)/scripts/sign-file"
-    #echo "DEBUG4: \$sign_file_binary_path = $sign_file_binary_path"
 
     path_virtualbox_modules_directory="$(dirname "$(modinfo -n vboxdrv)")"
-    #echo "DEBUG5: \$path_virtualbox_modules_directory = $path_virtualbox_modules_directory"
 
     # For each file in the VirtualBox kernel module directory
     for file_path_in_virtualbox_modules_directory in "$path_virtualbox_modules_directory"/*; do
@@ -48,17 +36,14 @@ function sign_virtualbox_modules() {
       # Decompressing XZ file
       if [ "${file_path_in_virtualbox_modules_directory: -3}" == ".xz" ]; then
         command_to_sign_kernel_module="$sign_file_binary_path sha256 \"$path_private_key\" \"$path_public_key\" \"$file_path_in_virtualbox_modules_directory\""
-        #echo "DEBUG7: \$command_to_sign_kernel_module = $command_to_sign_kernel_module"
         eval "$command_to_sign_kernel_module"
         xz --decompress --keep "$file_path_in_virtualbox_modules_directory"
         file_path_in_virtualbox_modules_directory="${file_path_in_virtualbox_modules_directory::-3}"
-        #echo "DEBUG6: \$file_path_in_virtualbox_modules_directory = $file_path_in_virtualbox_modules_directory"
       fi
 
       # Signing .ko file
       if [ "${file_path_in_virtualbox_modules_directory: -3}" == ".ko" ]; then
         command_to_sign_kernel_module="$sign_file_binary_path sha256 \"$path_private_key\" \"$path_public_key\" \"$file_path_in_virtualbox_modules_directory\""
-        #echo "DEBUG7: \$command_to_sign_kernel_module = $command_to_sign_kernel_module"
         eval "$command_to_sign_kernel_module"
       fi
 
