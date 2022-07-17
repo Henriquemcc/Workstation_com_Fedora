36309cdce1286375688cad39a08468b76709485e
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-07-17 14:36:59 -0300

    Adding the check if Weka directory already exists.

--- Install-Weka.bash
+++ Install-Weka.bash
@@ -6,33 +6,36 @@ download_file_name="weka-${version}-azul-zulu-linux.zip"
 url_weka_zip_file="https://prdownloads.sourceforge.net/weka/${download_file_name}"
 path_download_file="/tmp/${download_file_name}"
 path_installation_dir="${HOME}/.local/share"
-path_weka_dir="${path_installation_dir}/weka-${version}"
-path_weka_bin="${path_weka_dir}/weka.sh"
-path_weka_icon="${path_weka_dir}/weka.ico"
 
-# Creating directories if they do not exist
-sudo mkdir -p "$(dirname "$path_download_file")"
-mkdir -p "$path_installation_dir"
+if ! [ -d "$path_installation_dir" ]; then
+  path_weka_dir="${path_installation_dir}/weka-${version}"
+  path_weka_bin="${path_weka_dir}/weka.sh"
+  path_weka_icon="${path_weka_dir}/weka.ico"
 
-# Installing requirements
-bash ./Install-Curl.bash
-bash ./Install-Unzip.bash
+  # Creating directories if they do not exist
+  sudo mkdir -p "$(dirname "$path_download_file")"
+  mkdir -p "$path_installation_dir"
 
-# Downloading Weka
-curl -L "$url_weka_zip_file" --output "$path_download_file"
+  # Installing requirements
+  bash ./Install-Curl.bash
+  bash ./Install-Unzip.bash
 
-# Unzipping weka
-unzip -o "$path_download_file" -d "$path_installation_dir"
+  # Downloading Weka
+  curl -L "$url_weka_zip_file" --output "$path_download_file"
 
-# Creating shortcut
-path_shortcut="${HOME}/.local/share/applications/Weka.desktop"
- {
-    echo "[Desktop Entry]"
-    echo "Version=1.5"
-    echo "Name=Weka"
-    echo "Type=Application"
-    echo "Exec=bash $path_weka_bin"
-    echo "Icon=$path_weka_icon"
-    echo "StartupNotify=true"
-    echo "Categories=Education;Science;"
-  } > "$path_shortcut"
+  # Unzipping weka
+  unzip -o "$path_download_file" -d "$path_installation_dir"
+
+  # Creating shortcut
+  path_shortcut="${HOME}/.local/share/applications/Weka.desktop"
+   {
+      echo "[Desktop Entry]"
+      echo "Version=1.5"
+      echo "Name=Weka"
+      echo "Type=Application"
+      echo "Exec=bash $path_weka_bin"
+      echo "Icon=$path_weka_icon"
+      echo "StartupNotify=true"
+      echo "Categories=Education;Science;"
+    } > "$path_shortcut"
+fi
