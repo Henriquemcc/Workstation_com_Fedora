b44205ff9e3b7ab455679172330ff0dfebdc9955
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-08-06 10:59:21 -0300

    - Making update script parallel. - Separating the execution between root and user commands.

--- Update-All.bash
+++ Update-All.bash
@@ -1,5 +1,28 @@
 #!/bin/bash
-sudo dnf upgrade --refresh --assumeyes
-sudo snap refresh
-sudo flatpak update --assumeyes
-flatpak update --assumeyes
+
+# Commands to run as root
+function run_with_root_privileges()
+{
+  dnf upgrade --refresh --assumeyes &
+  snap refresh &
+  flatpak update --assumeyes &
+}
+
+# Command to run as user
+function run_without_root_privileges()
+{
+  flatpak update --assumeyes &
+}
+
+# Detecting whether it is running as root or user
+if [ "$(whoami)" == "root" ]; then
+    run_without_root_privileges
+
+    # Running as non-root user
+    sudo -u "$SUDO_USER" bash -c "$(declare -f run_without_root_privileges); run_without_root_privileges"
+else
+  # Running as root
+  sudo bash -c "$(declare -f run_with_root_privileges); run_with_root_privileges"
+
+  run_without_root_privileges
+fi
