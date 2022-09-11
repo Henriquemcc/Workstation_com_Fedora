ae7e1ada5cd777d4b9af0d76c8360ea8133e4d9d
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-09-11 18:13:11 -0300

    Adding the command to install Snap Core.

--- Install-Snapd.bash
+++ Install-Snapd.bash
@@ -1,7 +1,34 @@
 #!/bin/bash
 
-sudo dnf install --assumeyes snapd
-sudo ln -s /var/lib/snapd/snap /snap
-sudo dnf install --assumeyes fuse
-sudo dnf install --assumeyes squashfuse
-sudo dnf install --assumeyes kernel-modules
+# Runs this script as root if it is not root.
+function run_as_root() {
+  if [ "$(whoami)" != "root" ]; then
+    echo "This script is not running as root"
+    echo "Elevating privileges..."
+    if [ "$(command -v sudo)" ]; then
+      sudo bash "$0"
+      exit $?
+    else
+      echo "Sudo is not installed"
+      exit 1
+    fi
+  fi
+}
+
+# Running as root
+run_as_root
+
+# Installing Snap package manager
+dnf install --assumeyes snapd
+ln -s /var/lib/snapd/snap /snap
+
+# Installing Kernel modules
+dnf install --assumeyes fuse
+dnf install --assumeyes squashfuse
+dnf install --assumeyes kernel-modules
+
+# Installing snap core
+snap install core
+
+# Updating snap core
+snap refresh core
