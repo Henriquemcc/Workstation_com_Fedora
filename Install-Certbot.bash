def0961fa0c3f67d873a2f94a900537e3f844ed4
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-09-11 18:30:56 -0300

    Adding script.

---
+++ Install-Certbot.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Runs this script as root if it is not root.
+function run_as_root() {
+  if [ "$(whoami)" != "root" ]; then
+    echo "This script is not running as root"
+    echo "Elevating privileges..."
+    if [ "$(command -v sudo)" ]; then
+      sudo bash "$0"
+      exit $?
+    else
+      echo "Sudo is not installed"
+      exit 1
+    fi
+  fi
+}
+
+# Running as root
+run_as_root
+
+# Installing requirements
+bash ./Install-Snapd.bash
+
+# Installing Certbot
+snap install --classic certbot
+
+# Preparing the Certbot command
+ln -s /snap/bin/certbot /usr/bin/certbot
