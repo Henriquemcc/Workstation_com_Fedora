89413c5993af181ef438290d3dab35dfb750e791
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2023-07-16 20:50:32 -0300

    - Fixing erros on Wireguard scripts. - Criando menu de configuração do Wireguard.

---
+++ Wireguard/Configure-Server.bash
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Runs this script as root if it is not root.
+function run_as_root() {
+  if [ "$(whoami)" != "root" ]; then
+    echo "This script is not running as root"
+    echo "Elevating privileges..."
+    if [ "$(command -v sudo)" ]; then
+      sudo bash "$0"
+      exit $?
+    else
+      echo "Sudo is not installed"
+      exit 1
+    fi
+  fi
+}
+
+# Running as root
+run_as_root
+
+# Creating wireguard configuration directory and changing the current directory to it
+mkdir -p /etc/wireguard
+cd /etc/wireguard || exit
+
+# Setting permission
+umask 077
+
+# Generating server configuration
+bash ./Generate-ServerConfiguration.bash
+
+# Copying server configuration file
+cp ./wg0.conf /etc/wireguard/wg0.conf
+
+# Enabling IP Forwarding
+if ! grep -q "net.ipv4.ip_forward=1" "/etc/sysctl.conf"; then
+  echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+fi
+if ! grep -q "net.ipv6.conf.all.forwarding=1" "/etc/sysctl.conf"; then
+  echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
+fi
+
+# Starting and enabling wireguard service
+systemctl enable wg-quick@wg0.service
+systemctl daemon-reload
+systemctl start wg-quick@wg0
---
+++ Wireguard/Install-Wireguard.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Installs Wireguard to both server and client
+
+# Runs this script as root if it is not root.
+function run_as_root() {
+  if [ "$(whoami)" != "root" ]; then
+    echo "This script is not running as root"
+    echo "Elevating privileges..."
+    if [ "$(command -v sudo)" ]; then
+      sudo bash "$0"
+      exit $?
+    else
+      echo "Sudo is not installed"
+      exit 1
+    fi
+  fi
+}
+
+# Running as root
+run_as_root
+
+# Installing Wireguard
+dnf --assumeyes install wireguard-tools
