8c52ad55bf66ff578d00776a583cc33dc5dcf7be
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-01-07 13:15:10 -0300

    Removing unnecessary use of sudo command

--- Install-Glib.bash
+++ Install-Glib.bash
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-sudo dnf install glib --assumeyes
+# Runs this script as root if it is not root.
+function run_as_root() {
+  if [ "$(whoami)" != "root" ]; then
+    echo "This script is not running as root"
+    echo "Elevating privileges..."
+    if [ "$(command -v sudo)" ]; then
+      sudo bash "$0"
+      exit $?
+    else
+      echo "Sudo is not installed"
+      exit 1
+    fi
+  fi
+}
+
+# Running as root
+run_as_root
+
+# Installing Glib
+dnf install glib --assumeyes
