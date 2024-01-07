1ed40c8496823e1e99554224d1006e3ab7224f91
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-01-07 13:09:10 -0300

    Adding the command to enable snap

--- Install-Snapd.bash
+++ Install-Snapd.bash
@@ -20,6 +20,11 @@ run_as_root
 
 # Installing Snap package manager
 dnf install --assumeyes snapd
+
+# Enabling Snapd
+systemctl enable --now snapd.socket
+
+# Enabling classic snap support
 ln -s /var/lib/snapd/snap /snap
 
 # Installing Kernel modules
