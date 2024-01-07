87d2bd5e0f8106df4c13b006058960eb5b5a9a2a
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-01-07 13:03:42 -0300

    Removing unnecessary use of sudo command

--- Install-NvidiaCudaToolkit.bash
+++ Install-NvidiaCudaToolkit.bash
@@ -1,17 +1,35 @@
 #!/bin/bash
 
+# Runs this script as root if it is not root.
+function run_as_root() {
+  if [ "$(whoami)" != "root" ]; then
+    echo "This script is not running as root"
+    echo "Elevating privileges..."
+    if [ "$(command -v sudo)" ]; then
+      sudo bash "$0"
+      exit $?
+    else
+      echo "Sudo is not installed"
+      exit 1
+    fi
+  fi
+}
+
+# Running as root
+run_as_root
+
 # Adding repository
-sudo dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/fedora37/x86_64/cuda-fedora37.repo
+dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/fedora37/x86_64/cuda-fedora37.repo
 
 # Removing conflicting packages
-sudo dnf --assumeyes remove egl-gbm
-sudo dnf --assumeyes remove xorg-x11-drv-nvidia-cuda-libs
+dnf --assumeyes remove egl-gbm
+dnf --assumeyes remove xorg-x11-drv-nvidia-cuda-libs
 
 # Installing Nvidia Cuda
-sudo dnf module install --allowerasing --refresh --assumeyes nvidia-driver:open-dkms
-sudo dnf --assumeyes --disablerepo="rpmfusion-nonfree*" install cuda
+dnf module install --allowerasing --refresh --assumeyes nvidia-driver:open-dkms
+dnf --assumeyes --disablerepo="rpmfusion-nonfree*" install cuda
 
 # Updating grub
-sudo grub2-mkconfig -o /etc/grub2.cfg
-sudo grub2-mkconfig -o /etc/grub2-efi.cfg
-sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+grub2-mkconfig -o /etc/grub2.cfg
+grub2-mkconfig -o /etc/grub2-efi.cfg
+grub2-mkconfig -o /boot/grub2/grub.cfg
