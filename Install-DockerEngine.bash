df8be3dccb626fb31ad31e30650da298af9c844f
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-01-07 12:59:54 -0300

    Removing the use of the convenience script to install Docker Engine

--- Install-DockerEngine.bash
+++ Install-DockerEngine.bash
@@ -1,17 +1,31 @@
 #!/bin/bash
 
-# Downloading convenience script
-curl -fsSL https://get.docker.com -o get-docker.sh
+# Runs this script as root if it is not root.
+function run_as_root() {
+  if [ "$(whoami)" != "root" ]; then
+    echo "This script is not running as root"
+    echo "Elevating privileges..."
+    if [ "$(command -v sudo)" ]; then
+      sudo bash "$0"
+      exit $?
+    else
+      echo "Sudo is not installed"
+      exit 1
+    fi
+  fi
+}
 
-# Running convenience script
-sudo sh get-docker.sh
+# Running as root
+run_as_root
 
-# Installing docker rootless
-dockerd-rootless-setuptool.sh install
+# Installing requirements
+dnf install --assumeyes dnf-plugins-core
 
-# Enabling docker service
-sudo systemctl enable docker.service
+# Adding Docker repository
+dnf config-manager --assumeyes --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 
-# Removing convenience script
-bash ./Install-GioTrash.bash
-gio trash ./get-docker.sh
+# Installing Docker Engine
+dnf install --assumeyes docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Enabling Docker
+systemctl enable --now docker
