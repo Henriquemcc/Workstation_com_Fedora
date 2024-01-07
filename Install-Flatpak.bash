31fa823ed40fab8884b7f992e68e49e2cdeb84d9
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-01-07 13:12:59 -0300

    Removing unnecessary use of sudo command

--- Install-Flatpak.bash
+++ Install-Flatpak.bash
@@ -1,2 +1,22 @@
 #!/bin/bash
-sudo dnf install --assumeyes flatpak
+
+# Runs this script as root if it is not root.
+function run_as_root() {
+  if [ "$(whoami)" != "root" ]; then
+    echo "This script is not running as root"
+    echo "Elevating privileges..."
+    if [ "$(command -v sudo)" ]; then
+      sudo bash "$0"
+      exit $?
+    else
+      echo "Sudo is not installed"
+      exit 1
+    fi
+  fi
+}
+
+# Running as root
+run_as_root
+
+# Installing flatpak
+dnf install --assumeyes flatpak
