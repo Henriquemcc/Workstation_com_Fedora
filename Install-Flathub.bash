1cf3ead5d70b4ced3ab0bafede6656c6e8e467b8
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-01-07 13:12:21 -0300

    Removing unnecessary use of sudo command

--- Install-Flathub.bash
+++ Install-Flathub.bash
@@ -1,7 +1,25 @@
 #!/bin/bash
 
+# Runs this script as root if it is not root.
+function run_as_root() {
+  if [ "$(whoami)" != "root" ]; then
+    echo "This script is not running as root"
+    echo "Elevating privileges..."
+    if [ "$(command -v sudo)" ]; then
+      sudo bash "$0"
+      exit $?
+    else
+      echo "Sudo is not installed"
+      exit 1
+    fi
+  fi
+}
+
+# Running as root
+run_as_root
+
 # Installing requirements
 bash ./Install-Flatpak.bash
 
 # Adding Flathub
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
