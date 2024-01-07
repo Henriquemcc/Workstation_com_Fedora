c9eadb10e1aa00e1d630a869ab994f282d8a4575
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-01-07 13:16:23 -0300

    Removing unnecessary use of sudo command

--- Install-GoogleEarth.bash
+++ Install-GoogleEarth.bash
@@ -1,2 +1,22 @@
 #!/bin/bash
-sudo dnf install --assumeyes https://dl.google.com/dl/earth/client/current/google-earth-pro-stable-current.x86_64.rpm
+
+# Runs this script as root if it is not root.
+function run_as_root() {
+  if [ "$(whoami)" != "root" ]; then
+    echo "This script is not running as root"
+    echo "Elevating privileges..."
+    if [ "$(command -v sudo)" ]; then
+      sudo bash "$0"
+      exit $?
+    else
+      echo "Sudo is not installed"
+      exit 1
+    fi
+  fi
+}
+
+# Running as root
+run_as_root
+
+# Installing Google Earth
+dnf install --assumeyes https://dl.google.com/dl/earth/client/current/google-earth-pro-stable-current.x86_64.rpm
