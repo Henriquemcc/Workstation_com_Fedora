4f1908e5988ceb903caa6b13880b9682b814e776
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-01-07 13:10:23 -0300

    Removing unnecessary use of sudo command

--- Disable-Wifi.bash
+++ Disable-Wifi.bash
@@ -1,2 +1,22 @@
 #!/bin/bash
-sudo nmcli radio wifi off
+
+# Runs this script as root if it is not root.
+function run_as_root() {
+  if [ "$(whoami)" != "root" ]; then
+    echo "This script is not running as root"
+    echo "Elevating privileges..."
+    if [ "$(command -v sudo)" ]; then
+      sudo bash "$0"
+      exit $?
+    else
+      echo "Sudo is not installed"
+      exit 1
+    fi
+  fi
+}
+
+# Running as root
+run_as_root
+
+# Disabling wifi
+nmcli radio wifi off
