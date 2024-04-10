1ed445a6b7d355cdc7b411e9db4c877f130df898
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-04-10 15:40:08 -0300

    Converting script to avoid to run on CentOS or RHEL

--- Upgrade-ToFedora35.bash
+++ Upgrade-ToFedora35.bash
@@ -1,7 +1,16 @@
 #!/bin/bash
 # Upgrades to Fedora 35
 
-sudo dnf upgrade --refresh --assumeyes
-sudo dnf install dnf-plugin-system-upgrade --assumeyes
-sudo dnf system-upgrade download --releasever=35 --assumeyes
-sudo dnf system-upgrade reboot --assumeyes
+# Importing function run_as_root and get_os_type
+source RunAsRoot.bash
+source OsInfo.bash
+
+# Running as root
+run_as_root
+
+if [ "$(get_os_type)" == "fedora" ]; then
+  dnf upgrade --refresh --assumeyes
+  dnf install dnf-plugin-system-upgrade --assumeyes
+  dnf system-upgrade download --releasever=35 --assumeyes
+  dnf system-upgrade reboot --assumeyes
+fi
