bd4cf8a0cf56a620f3d47b089aae7f27433092cc
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-04-10 15:47:34 -0300

    Converting script to avoid to run on CentOS or RHEL

--- UpgradeFedora.bash
+++ UpgradeFedora.bash
@@ -1,14 +1,26 @@
 #!/bin/bash
 
+# Importing function run_as_root and get_os_type
+source OsInfo.bash
+
+# Exiting menu if the operating system is not fedora
+if [ "$(get_os_type)" != "fedora" ]; then
+  exit  1
+fi
+
 function_return_variable=
 
 function obter_opcao() {
   _opcao_selecionada=-1
   re='^[0-9]+$'
-  while ! [[ $_opcao_selecionada =~ $re ]] || [ $_opcao_selecionada -lt 0 ] || [ $_opcao_selecionada -gt 1 ]; do
+  while ! [[ $_opcao_selecionada =~ $re ]] || [ $_opcao_selecionada -lt 0 ] || [ $_opcao_selecionada -gt 5 ]; do
     echo "O que deseja fazer?"
     echo "0 - Sair"
     echo "1 - Atualizar para o Fedora 35"
+    echo "2 - Atualizar para o Fedora 36"
+    echo "3 - Atualizar para o Fedora 37"
+    echo "4 - Atualizar para o Fedora 38"
+    echo "5 - Atualizar para o Fedora 39"
     read -r _opcao_selecionada
   done
 
@@ -23,6 +35,14 @@ while [ $opcao_selecionada -ne 0 ]; do
   opcao_selecionada="$function_return_variable"
 
   if [ "$opcao_selecionada" -eq 1 ]; then
-    bash ./Upgrade-ToFedora35.bash
+    bash Upgrade-FedoraToASpecificVersion.bash 35
+  elif [ "$opcao_selecionada" -eq 2 ]; then
+    bash Upgrade-FedoraToASpecificVersion.bash 36
+  elif [ "$opcao_selecionada" -eq 3 ]; then
+    bash Upgrade-FedoraToASpecificVersion.bash 37
+  elif [ "$opcao_selecionada" -eq 4 ]; then
+    bash Upgrade-FedoraToASpecificVersion.bash 38
+  elif [ "$opcao_selecionada" -eq 5 ]; then
+    bash Upgrade-FedoraToASpecificVersion.bash 39
   fi
 done
