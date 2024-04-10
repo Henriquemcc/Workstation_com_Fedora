e6cbbf700d8df782122fe050769d5ceb54cbf47a
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-04-10 15:38:23 -0300

    Converting script to be compatible with RHEL and CentOS

--- Install-MegaSync.bash
+++ Install-MegaSync.bash
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-# Importing function run_as_root
+# Importing function run_as_root, get_os_type and get_os_version
 source RunAsRoot.bash
+source OsInfo.bash
 
 # Running as root
 run_as_root
 
 # Installation variables
-VERSION_ID="$(awk -F= '$1=="VERSION_ID" { print $2 ;}' /etc/os-release)"
+VERSION_ID="$(get_os_version)"
 ARCHITECTURE="$(uname -m)"
 
 # Installing Mega Sync
-dnf install --assumeyes "https://mega.nz/linux/repo/Fedora_${VERSION_ID}/${ARCHITECTURE}/megasync-Fedora_${VERSION_ID}.${ARCHITECTURE}.rpm"
+if [ "$(get_os_type)" == "fedora" ]; then
+  dnf install --assumeyes "https://mega.nz/linux/repo/Fedora_${VERSION_ID}/${ARCHITECTURE}/megasync-Fedora_${VERSION_ID}.${ARCHITECTURE}.rpm"
+elif [ "$(get_os_type)" == "centos" ] || [ "$(get_os_type)" == "rhel" ]; then
+  dnf install --assumeyes https://mega.nz/linux/repo/Fedora_39/x86_64/megasync-Fedora_39.x86_64.rpm
+fi
