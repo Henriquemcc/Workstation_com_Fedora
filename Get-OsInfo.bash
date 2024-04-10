966f6e1d13affa49d368b439c32adefd0799843e
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-04-10 10:38:47 -0300

    Adding script

---
+++ Get-OsInfo.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+os_release_file="/etc/os-release"
+
+# Get the version of the operating system
+function get_os_version() {
+  if [ -f $os_release_file ]; then
+      grep "VERSION_ID=" /etc/os-release | sed 's/VERSION_ID=//'
+  else
+    echo "Unknown distribution or version."
+    exit 1
+  fi
+}
+
+# Get the type of the operating system
+function get_os_type() {
+  if [ -f $os_release_file ]; then
+      grep "^ID=" /etc/os-release | sed 's/ID=//'
+  else
+    echo "Unknown distribution or version."
+    exit 1
+  fi
+}
