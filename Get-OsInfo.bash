d33aa98221b2edb2316d2b3b762839f020a50216
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-04-10 14:08:59 -0300

    Fixing functions get_os_version and get_os_type

--- Get-OsInfo.bash
+++ Get-OsInfo.bash
@@ -5,7 +5,7 @@ os_release_file="/etc/os-release"
 # Get the version of the operating system
 function get_os_version() {
   if [ -f $os_release_file ]; then
-      grep "VERSION_ID=" /etc/os-release | sed 's/VERSION_ID=//; s/"//g' | awk -F'.' '{print $1}'
+      grep "^VERSION_ID=" /etc/os-release | sed 's/VERSION_ID=//; s/"//g' | awk -F'.' '{print $1}'
   else
     echo "Unknown distribution or version."
     exit 1
@@ -15,8 +15,7 @@ function get_os_version() {
 # Get the type of the operating system
 function get_os_type() {
   if [ -f $os_release_file ]; then
-      grep "^ID=" /etc/os-release | sed 's/ID="//; s/"//g'
-
+      grep "^ID=" /etc/os-release | sed 's/ID=//; s/"//g'
   else
     echo "Unknown distribution or version."
     exit 1
