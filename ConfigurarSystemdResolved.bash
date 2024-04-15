7e475974761ba1b00c4ab465326c492c941953a8
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-04-15 10:36:52 -0300

    Fixing erro in which systemd-resolved service was not enabled

--- ConfigurarSystemdResolved.bash
+++ ConfigurarSystemdResolved.bash
@@ -9,6 +9,9 @@ run_as_root
 # Installing systemd-resolved
 dnf install --assumeyes systemd-resolved
 
+# Enabling systemd-resolved service
+systemctl enable --now systemd-resolved.service
+
 # Backing up configuration file
 cp "/etc/systemd/resolved.conf" "/etc/systemd/resolved.conf.backup.$(date "+%d-%m-%Y_%H:%M:%S")"
 
