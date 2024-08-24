a83a1820ba2e7eb7f40144ea2b17e5e0e69fa61e
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-08-24 16:07:51 -0300

    - Adicionando servidores adicionais - Salvando em dois arquivos

--- ConfigurarNtp.bash
+++ ConfigurarNtp.bash
@@ -11,6 +11,7 @@ dnf install --assumeyes chrony
 
 # Backing up configuration file
 cp "/etc/chrony/chrony.conf" "/etc/chrony/chrony.conf.backup.$(date "+%d-%m-%Y_%H:%M:%S")"
+cp "/etc/chrony.conf" "/etc/chrony.conf.backup.$(date "+%d-%m-%Y_%H:%M:%S")"
 
 # Generating new configuration file
 {
@@ -23,8 +24,8 @@ cp "/etc/chrony/chrony.conf" "/etc/chrony/chrony.conf.backup.$(date "+%d-%m-%Y_%
 
 	# caso deseje pode configurar servidores adicionais com NTS, como os da cloudflare e netnod
 	# nesse caso basta descomentar as linhas a seguir
-	#echo "server time.cloudflare.com iburst nts"
-	#echo "server nts.netnod.se iburst nts"
+	echo "server time.cloudflare.com iburst nts"
+	echo "server nts.netnod.se iburst nts"
 
 	# arquivo usado para manter a informação do atraso do seu relógio local
 	echo "driftfile /var/lib/chrony/chrony.drift"
@@ -50,7 +51,7 @@ cp "/etc/chrony/chrony.conf" "/etc/chrony/chrony.conf.backup.$(date "+%d-%m-%Y_%
 	# diretiva que indica que o offset UTC e leapseconds devem ser lidos
 	# da base tz (de time zone) do sistema
 	echo "leapsectz right/UTC"
-} > "/etc/chrony/chrony.conf"
+} | tee "/etc/chrony.conf" "/etc/chrony/chrony.conf" > /dev/null
 
 # Enabling and restarting chrony
 systemctl enable --now chronyd.service
