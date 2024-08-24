396e54ac22c5f58c5ead22652905b6046e62582d
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-08-24 15:48:30 -0300

    Substituindo o systemd-timesyncd pelo chrony

--- ConfigurarNtp.bash
+++ ConfigurarNtp.bash
@@ -6,21 +6,52 @@ source RunAsRoot.bash
 # Running as root
 run_as_root
 
-# Installing systemd-timesyncd
-dnf install --assumeyes systemd-timesyncd
-
-# Enabling NTP service
-systemctl enable systemd-timesyncd
-timedatectl set-ntp yes
+# Installing chrony
+dnf install --assumeyes chrony
 
 # Backing up configuration file
-cp "/etc/systemd/timesyncd.conf" "/etc/systemd/timesyncd.conf.backup.$(date "+%d-%m-%Y_%H:%M:%S")"
+cp "/etc/chrony/chrony.conf" "/etc/chrony/chrony.conf.backup.$(date "+%d-%m-%Y_%H:%M:%S")"
 
 # Generating new configuration file
 {
-  echo "[Time]"
-  echo "NTP=a.st1.ntp.br b.st1.ntp.br c.st1.ntp.br d.st1.ntp.br gps.ntp.br a.ntp.br b.ntp.br c.ntp.br" # Using NTP.br servers
-} > "/etc/systemd/timesyncd.conf"
+	# servidores publicos do NTP.br com NTS disponível
+	echo "server a.st1.ntp.br iburst nts"
+	echo "server b.st1.ntp.br iburst nts"
+	echo "server c.st1.ntp.br iburst nts"
+	echo "server d.st1.ntp.br iburst nts"
+	echo "server gps.ntp.br iburst nts"
 
-# Restarting NTP service
-systemctl restart systemd-timesyncd
+	# caso deseje pode configurar servidores adicionais com NTS, como os da cloudflare e netnod
+	# nesse caso basta descomentar as linhas a seguir
+	#echo "server time.cloudflare.com iburst nts"
+	#echo "server nts.netnod.se iburst nts"
+
+	# arquivo usado para manter a informação do atraso do seu relógio local
+	echo "driftfile /var/lib/chrony/chrony.drift"
+
+	# local para as chaves e cookies NTS
+	echo "ntsdumpdir /var/lib/chrony"
+
+	# se quiser um log detalhado descomente as linhas a seguir
+	#echo "log tracking measurements statistics"
+	#echo "logdir /var/log/chrony"
+
+	# erro máximo tolerado em ppm em relação aos servidores
+	echo "maxupdateskew 100.0"
+
+	# habilita a sincronização via kernel do real-time clock a cada 11 minutos
+	echo "rtcsync"
+
+	# ajusta a hora do sistema com um "salto", de uma só vez, ao invés de
+	# ajustá-la aos poucos corrigindo a frequência, mas isso apenas se o erro
+	# for maior do que 1 segundo e somente para os 3 primeiros ajustes
+	echo "makestep 1 3"
+
+	# diretiva que indica que o offset UTC e leapseconds devem ser lidos
+	# da base tz (de time zone) do sistema
+	echo "leapsectz right/UTC"
+} > "/etc/chrony/chrony.conf"
+
+# Enabling and restarting chrony
+systemctl enable --now chronyd.service
+systemctl restart chronyd.service
